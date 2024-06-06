43b090ca130ce45f346689129f2a45fd19737e90
Damian Hall <damian@damianhall.net>
2024-06-06 16:20:12 -0400

    end of lesson 8

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', quantity: 50, onSale: true },
+              { id: 2235, color: 'blue', quantity: 0, onSale: false },
+              { id: 2235, color: 'purple', quantity: 10, onSale: true },
             ]
         }
     },
@@ -17,8 +17,19 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(variantIndex) {
+            this.selectedVariant = variantIndex;
+        }
+    },
+    computed: {
+        title() {
+            return `${this.variants[this.selectedVariant].onSale ? 'On Sale: ': ''}${this.brand} ${this.product}`;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0;
+        },
+        image() {
+            return `./assets/images/socks_${this.variants[this.selectedVariant].color}.jpg`;
         }
     }
 })
